2b242b46b5146ca9c0eb37083d1a16adf8a755ce
dot-cera <ritaclaris980@gmail.com>
2024-01-08 14:09:02 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+ customerName = customerName.toUpperCase();
+return customerName;
+}
+
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = "not bob";
+return bestCustomer;
+}
+
+function overwriteBestCustomer(best){
+    bestCustomer = best;
+    return bestCustomer;
+}
+const leastFavoriteCustomer = "claris";
+function changeLeastFavoriteCustomer(least){
+    leastFavoriteCustomer =least;
+    return leastFavoriteCustomer;
+}
